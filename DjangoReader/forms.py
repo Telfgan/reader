d6623eebf96f27ce4866cb1351271d504e3f6b54
Infinity <n.dima.1998@mail.ru>
2023-04-09 00:47:40 +0300

    Добавил стили для формы авторизации.

--- DjangoReader/forms.py
+++ DjangoReader/forms.py
@@ -6,8 +6,8 @@ from .models import Image, Language
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
 
 
 class RegisterForm(UserCreationForm):
